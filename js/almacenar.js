217db4823a8247cb173cb293fade85354955e712
Rodrigo Martinez <ropecos94@gmail.com>
2023-08-23 13:20:02 -0300

    Update almacenar.js

---
+++ js/almacenar.js
@@ -0,0 +1,7 @@
+const inputText = document.getElementById('item');
+const botonAgregar = document.getElementById('agregar');
+const displayText = document.getElementById('contenedor');
+
+botonAgregar.addEventListener('click', function(){
+    inputText.value.innerHTML()
+})
