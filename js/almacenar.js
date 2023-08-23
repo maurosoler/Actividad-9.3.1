22de93dea3a66b4b852126054704167017321cf4
Rodrigo Martinez <ropecos94@gmail.com>
2023-08-23 13:53:10 -0300

    Update almacenar.js

--- js/almacenar.js
+++ js/almacenar.js
@@ -2,12 +2,14 @@ const inputText = document.getElementById('item');
 const botonAgregar = document.getElementById('agregar');
 const displayText = document.getElementById('contenedor');
 const items = localStorage.getItem('items');
+const array = [];
 
 botonAgregar.addEventListener('click', function(){
     const valorInput = inputText.value;
 
     if(valorInput !== ''){
         displayText.innerHTML += `<li>${valorInput}</li>`;
+        array.push(valorInput);
         localStorage.setItem('items',valorInput);
     }
     inputText.value ='';
